bbe98ec9dc4c404939eb9a843f5a3dc3698fef46
Abdelkader Lounis <abdouaallb@gmail.com>
2021-12-18 00:49:34 +0100

    third commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,41 @@
-$(document).ready(function () {
-  $("#spanm").on("click", function () {
-    $(".detailsImg").addClass("rotate");
+$(function () {
+  $("#spanm").each(function (index) {
+    $(this).on("click", function () {
+      $(".detailsImg").removeClass("rotatereverce");
+      $(".detailsImg").addClass("rotate");
+      $(".pizzaImg").removeClass("pizzaDefaultDim");
+      $(".pizzaImg").addClass("pizzaEditedDim");
+    });
   });
 });
+
+$(function () {
+  $("#spane").each(function () {
+    $(this).on("click", function () {
+      $(".detailsImg").removeClass("rotate");
+      $(".detailsImg").addClass("rotatereverce");
+      $(".pizzaImg").removeClass("pizzaEditedDim");
+      $(".pizzaImg").addClass("pizzaDefaultDim");
+    });
+  });
+});
+
+$(function () {
+  $("#spanz").each(function () {
+    $(this).on("click", function () {
+      $(".tooping1").css({ width: "154px" });
+    });
+  });
+});
+
+$(function () {
+  $("#spanc").each(function () {
+    $(this).on("click", function () {
+      $(".tooping3").css({ width: "190px" });
+    });
+  });
+});
+
+function takeMeToThanks() {
+  location.href = "/thanks.html";
+}
